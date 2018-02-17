05bf43a4e9d6af1fc910fe4702953357519c9664
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2018-02-17 18:03:52 +0100

    Add the search field as a component.

---
+++ src/components/search_bar.js
@@ -0,0 +1,7 @@
+//there is a need to include the import of react as any JSX will be converted to React.createElment in JavaScript without it there will be an error
+import React from 'react';
+
+//create a search component for recieving query search terms.
+const SearchBar = () => {
+    return <input />;
+};
