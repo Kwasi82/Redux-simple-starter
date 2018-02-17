6465ebe67ead25a0649aeacc8c4a80052fb75e12
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2018-02-17 15:16:02 +0100

    Add component.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 // Create a new component. It should produce some HTML
 
+
+// const represents a constant that never changes and is a variation on variable. It is ES6
+const App = function() {
+    return <div>Hi!</div>;
+}
+
 // Take this component's generated HTML and put it on the page
