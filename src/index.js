c0a4e4b65254d955495b220106339f3aa1b61b8c
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2018-02-17 16:53:20 +0100

    Import react library and render to the DOM.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-// Create a new component. It should produce some HTML
+//This calls the library 'react' from the node modules folder and assigns it to variable 'React'
+import React from 'react';
 
+// Create a new component. It should produce some HTML
 
 // const represents a constant that never changes and is a variation on variable. It is ES6
 const App = function() {
     return <div>Hi!</div>; // returnig what looks like HTML in JavaScript is JSX.
 }
 
-// Take this component's generated HTML and put it on the page
+// Take this component's generated HTML and put it on the page (in the DOM)
+React.render(App);
