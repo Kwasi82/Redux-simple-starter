569768030c09381bede89dfec600d342256a6c16
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2018-02-17 15:18:48 +0100

    Add comment to component.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 // const represents a constant that never changes and is a variation on variable. It is ES6
 const App = function() {
-    return <div>Hi!</div>;
+    return <div>Hi!</div>; // returnig what looks like HTML in JavaScript is JSX.
 }
 
 // Take this component's generated HTML and put it on the page
