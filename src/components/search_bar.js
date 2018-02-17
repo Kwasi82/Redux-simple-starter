a548b30793065e73b5bda67caeef195b87cd61fa
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2018-02-17 18:53:34 +0100

    Add a comment about the SearchBar component.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,7 @@
 //there is a need to include the import of react as any JSX will be converted to React.createElment in JavaScript without it there will be an error
 import React from 'react';
 
-//create a search component for recieving query search terms.
+//create a search component for recieving query search terms. This is described as a 'functional component' because it is a function.
 const SearchBar = () => {
     return <input />;
 };
