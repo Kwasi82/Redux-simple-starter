526730a9e105f78abdeee292a1522d3ca81edf90
Kwasi Adutwum Asiedu <kwasi.asiedu@gmail.com>
2018-02-17 18:55:59 +0100

    Comment out SearchBar functional component.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,10 @@
 import React from 'react';
 
 //create a search component for recieving query search terms. This is described as a 'functional component' because it is a function.
-const SearchBar = () => {
+/* const SearchBar = () => {
     return <input />;
 };
+*/
 
 //This provides the SearchBar component to other files. It needs to be imported to files that may need it.
 export default SearchBar;
